b820be005b806961d3228bd972174447394a9e11
Ihab Adly <ihab.adly@yahoo.com>
2019-08-10 13:02:43 +0200

    Update subtractor.vhd

--- subtractor.vhd
+++ subtractor.vhd
@@ -4,16 +4,12 @@ use ieee.std_logic_arith.all;
 
 entity subtractor is
   port(x,y: in std_logic_vector (7 downto 0);
-    z: out std_logic_vector (7 downto 0)
-  );
+    z: out std_logic_vector (7 downto 0));
 end subtractor ;
 
 architecture rtl of subtractor is
   signal z_int : unsigned(7 downto 0);
 begin
-  process(x,y)
-  begin
-    z_int <= unsigned(x) - unsigned(y);
-  end process;
-    z <= std_logic_vector(z_int);
+  z_int <= unsigned(x) - unsigned(y);
+  z <= std_logic_vector(z_int);
 end rtl;
